190bbc6babe3e422fea18b7ab638dce6d8cdf774
Yohei Onishi <vivre214@gmail.com>
2014-01-31 23:47:06 -0800

    add q2 for social db

--- 06_SQL/SQL_Social_Network_Query_Exercises_Core_Set/q2.sql
+++ 06_SQL/SQL_Social_Network_Query_Exercises_Core_Set/q2.sql
@@ -1,9 +1,12 @@
 -- Q2 For every student who likes someone 2 or more grades younger than themselves,
 -- return that student's name and grade, and the name and grade of the student they like. 
 
-select *
-from (select *
-      from Likes l1 join Highschooler h1 on h1.ID = l1.ID1),
-     (select *
+select h1name, h1grade, h2name, h2grade
+from (select l1.ID1 as l1ID1, h1.name as h1name, h1.grade as h1grade
+      from Likes l1 join Highschooler h1 on h1.ID = l1.ID1) join
+     (select l2.ID1 as l2ID1, l2.ID2 as l2ID2, h2.name as h2name, h2.grade as h2grade 
       from Likes l2 join Highschooler h2 on h2.ID = l2.ID2)
-where l1.ID2 = l2.ID1;
+     on l1ID1 = l2ID1
+where (h1grade - h2grade) >= 2;
+
+
