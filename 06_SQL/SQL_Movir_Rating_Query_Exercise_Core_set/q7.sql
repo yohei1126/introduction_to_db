cc8283031ca76d6996f95f0a79adfa52222a234f
Yohei Onishi <vivre214@gmail.com>
2014-01-31 22:44:48 -0800

    final answer to q7 for movie rating

--- 06_SQL/SQL_Movir_Rating_Query_Exercise_Core_set/q7.sql
+++ 06_SQL/SQL_Movir_Rating_Query_Exercise_Core_set/q7.sql
@@ -1,9 +1,8 @@
 -- Q7. For each movie that has at least one rating, find the highest number of stars that movie received.
 -- Return the movie title and number of stars. Sort by movie title. 
-SELECT name, title
-FROM Reviewer, Movie
-WHERE exists (
- SELECT *
- from Rating R1, Rating R2
- WHERE (R1.rID = R2.rID and R1.mID = R2.mID and R1.ratingDate < R2.ratingDate and R1.stars < R2.stars) and R1.rID = Reviewer.rID and R1.mID = Movie.mID);
+
+SELECT title, max(r1.stars)
+FROM (Rating r1 join Rating r2 on r1.mID = r2.mID and r1.stars > r2.stars) join Movie on r1.mID = Movie.mID
+group by r1.mID
+order by title;
 
